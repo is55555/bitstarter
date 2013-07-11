878c5277a5a986bc06716c42d57b7c3e49ae3b67
I.S <muyuubyou@gmail.com>
2013-07-11 22:46:33 +0100

    grader still not behaving

--- graderStr.js
+++ graderStr.js
@@ -28,7 +28,6 @@ var rest    = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 var FNAME = "downloaded_url.html";
-var sleep = require('sleep');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -43,8 +42,23 @@ var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+var cheerioString = function(htmlString) {
+    return cheerio.load(htmlString);
+}
+
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
+};
+
+var checkHtmlString = function(htmlString, checksfile) {
+    $ = cheerioString(htmlString);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
@@ -68,19 +82,16 @@ var download_url = function(url, fname) {
     rest.get(url).on('complete', function(result) {
         if (result instanceof Error) {
 	console.log('error: ' + result.message);
-        //return false;
         process.exit(1);
-	} else {
-        fs.writeFileSync(fname, result)
-        //fs.writeFileSync(fname, result, function(err) {
-	//	if(err) throw err;
-          //      console.log("saved");
-	//	});
-       console.log("finished download");
-       return;
-       }
+	}
+        
+	
+	fs.writeFileSync(fname, result, function(err) {
+		if(err) throw err;
+                console.log("saved");
+		});
     });
-    do {sleep.sleep(1); console.log("*");} while(true);
+    console.log("finished download");
 }
 
 if(require.main == module) {
